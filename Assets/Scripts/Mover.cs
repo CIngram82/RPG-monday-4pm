ebdb5ded74baf7c943dc2be46e83f19612496bb4
christopher ingram <cingram82@gmail.com>
2020-08-17 19:42:15 -0400

    add

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,13 +6,6 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
