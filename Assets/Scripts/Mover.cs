930e1dfb875f154402d55ff2591123cde6ddd1ff
christopher ingram <cingram82@gmail.com>
2020-08-24 17:52:23 -0400

    Update move
    Player moves on Right click now

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,10 +8,11 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             MoveToCursorClick();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursorClick()
@@ -23,5 +24,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
     }
 }
